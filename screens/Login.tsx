46e53833c16a9eb5c50200dc107b726d6c55ca82
Norman Munge <normanmunge@gmail.com>
2022-07-27 06:38:59 +0300

    Outlines login screen structure

---
+++ screens/Login.tsx
@@ -0,0 +1,15 @@
+import { View, TextInput, Text } from 'react-native';
+
+function LoginScreen() {
+  return (
+    <View>
+      <TextInput /> {/* TODO: Create custom form input: email/phone */}
+      <TextInput /> {/* TODO: Create custom form input: password */}
+      LOGIN BUTTONS {/* TODO: Customise button */}
+      <Text /> {/* create new account */}
+      <Text /> {/* forgot password */}
+    </View>
+  );
+}
+
+export default LoginScreen;
