92863d3d942100bdc969cbb47e263240e03d6e34
Norman Munge <normanmunge@gmail.com>
2022-08-01 07:58:27 +0300

    Styling primary btn component

--- components/buttons/button.tsx
+++ components/buttons/button.tsx
@@ -1,15 +1,47 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 type ButtonProps = {
   //text: string;
   //rest of the props
 };
 const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
+  const { btnInnerContainer, btnOutterContainer, btnPressed, btnText } = styles;
+  const onPressHandler = () => {
+    console.log('PRESSED!');
+  };
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={btnOutterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed ? [btnInnerContainer, btnPressed] : btnInnerContainer
+        }
+        onPress={onPressHandler}
+      >
+        <Text style={btnText}>{children}</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default Button;
+
+const styles = StyleSheet.create({
+  btnOutterContainer: {
+    borderRadius: 6,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  btnInnerContainer: {
+    backgroundColor: '',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    elevation: 2,
+  },
+  btnText: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+  btnPressed: {
+    opacity: 0.75,
+  },
+});
