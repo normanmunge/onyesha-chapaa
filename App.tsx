4e84bcd82fc4fc328259088e2d4f2043d1d41e44
Norman Munge <normanmunge@gmail.com>
2022-08-17 17:25:41 +0300

    Adds app ready dummy user token check

--- App.tsx
+++ App.tsx
@@ -1,8 +1,42 @@
 import { StyleSheet } from 'react-native';
+import { useState } from 'react';
 import LoginScreen from './screens/authentication/Login';
+// import { NavigationContainer } from '@react-navigation/native';
+
+import { StatusBar } from 'expo-status-bar';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import RecentExpensesScreen from './screens/expenses/recent';
+import ManageExpenseScreen from './screens/expenses/manage';
+import AppNavigator from './navigation/app.navigator';
+
+// const Stack = createNativeStackNavigator();
+// const BottomTabs = createBottomTabNavigator();
 
 export default function App() {
-  return <LoginScreen />;
+  const [appIsReady, setAppIsReady] = useState<boolean>(false);
+
+  // if (!appIsReady) {
+  //   return null;
+  // }
+
+  const userToken = false;
+
+  return (
+    <>
+      <StatusBar style='auto' />
+
+      {userToken ? ( //user not signed in
+        // <Stack.Screen name='SignIn' component={LoginScreen} options={{}} />
+
+        <LoginScreen />
+      ) : (
+        //user signed in
+        <AppNavigator />
+      )}
+    </>
+  );
+  // return <LoginScreen />;
 }
 
 const styles = StyleSheet.create({});
