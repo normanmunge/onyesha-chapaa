cc15232d2a4f41109e71282f80c46b00695058da
Norman Munge <normanmunge@gmail.com>
2022-08-17 17:26:05 +0300

    Removes unnecessary styles

--- screens/authentication/Login.tsx
+++ screens/authentication/Login.tsx
@@ -4,11 +4,7 @@ import PrimaryButton from '../../components/ui/buttons/primary-button';
 function LoginScreen() {
   return (
     <View>
-      <TextInput /> {/* TODO: Create custom form input: email/phone */}
-      <TextInput /> {/* TODO: Create custom form input: password */}
       <PrimaryButton>LOGIN</PrimaryButton>
-      <Text /> {/* create new account */}
-      <Text /> {/* forgot password */}
     </View>
   );
 }
