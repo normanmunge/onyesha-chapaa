8e39ec934e77609c12809e862f53bc141cd6d126
Norman Munge <normanmunge@gmail.com>
2022-09-05 09:22:06 +0300

    Checks manage expense screen whether adding or editing item

--- screens/expenses/manage.tsx
+++ screens/expenses/manage.tsx
@@ -1,6 +1,22 @@
+import { useLayoutEffect } from 'react';
 import { View, Text } from 'react-native';
 
-const ManageExpenseScreen = () => {
+const ManageExpenseScreen = ({
+  route,
+  navigation,
+}: {
+  route: any;
+  navigation: any;
+}) => {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? 'Edit Expense' : 'Add Expense',
+    });
+  }, [navigation, isEditing]);
+
   return (
     <View>
       <Text>This is the screen to manage an expense</Text>
