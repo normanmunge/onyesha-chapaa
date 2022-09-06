9961a6ba85076669987d28864c004dfc27672ee6
Norman Munge <normanmunge@gmail.com>
2022-09-06 08:25:44 +0300

    Adds expense management closing of modal

--- screens/expenses/manage.tsx
+++ screens/expenses/manage.tsx
@@ -1,5 +1,8 @@
 import { useLayoutEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import IconButton from '../../components/ui/buttons/icon-button';
+import PrimaryButton from '../../components/ui/buttons/primary-button';
+import { theme } from '../../constants/theme';
 
 const ManageExpenseScreen = ({
   route,
@@ -11,17 +14,74 @@ const ManageExpenseScreen = ({
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
 
+  const { container, deleteContainer, buttonsContainer, button } = styles;
+
   useLayoutEffect(() => {
     navigation.setOptions({
       title: isEditing ? 'Edit Expense' : 'Add Expense',
     });
   }, [navigation, isEditing]);
 
+  const deleteExpenseHandler = () => {
+    navigation.goBack();
+  };
+  const cancelHandler = () => {
+    navigation.goBack();
+  };
+  const confirmHandler = () => {
+    navigation.goBack();
+  };
+
   return (
-    <View>
-      <Text>This is the screen to manage an expense</Text>
+    <View style={container}>
+      <View style={buttonsContainer}>
+        <PrimaryButton
+          mode='flat'
+          styleProp={button}
+          onPressHandler={cancelHandler}
+        >
+          Cancel
+        </PrimaryButton>
+        <PrimaryButton styleProp={button} onPressHandler={confirmHandler}>
+          {isEditing ? 'Update' : 'Add'}
+        </PrimaryButton>
+      </View>
+      {isEditing && (
+        <View style={deleteContainer}>
+          <IconButton
+            icon='trash'
+            color={theme.colors.error}
+            size={36}
+            onPressHandler={deleteExpenseHandler}
+          />
+        </View>
+      )}
     </View>
   );
 };
 
 export default ManageExpenseScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: theme.colors.primary_darker,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: theme.colors.primary_lighter,
+    alignItems: 'center',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+});
