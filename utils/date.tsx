afac1976fb341ff9aca20dbacaeb5364bc40d8d7
Norman Munge <normanmunge@gmail.com>
2022-09-07 07:59:09 +0300

    Creates new utility to get date minus number of passed days

--- utils/date.tsx
+++ utils/date.tsx
@@ -1,5 +1,10 @@
-export function getFormattedDate(date:Date) {
-    const month = date.getMonth() + 1 > 9 ? date.getMonth() + 1: `0${date.getMonth() + 1}`;
-    const day = date.getDay() > 9 ? date.getDay(): `0${date.getDay()}` 
-    return `${date.getFullYear()}-${month}-${day}`
+export function getFormattedDate(date: Date) {
+  const month =
+    date.getMonth() + 1 > 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`;
+  const day = date.getDay() > 9 ? date.getDay() : `0${date.getDay()}`;
+  return `${date.getFullYear()}-${month}-${day}`;
+}
+
+export function getDateMinusDays(date: Date, days: number) {
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate() - days);
 }
