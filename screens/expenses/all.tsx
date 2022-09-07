5cda344d01ef2dd63f11515fac8b21d63fe749da
Norman Munge <normanmunge@gmail.com>
2022-09-07 08:00:13 +0300

    Gets all expenses from expenses context

--- screens/expenses/all.tsx
+++ screens/expenses/all.tsx
@@ -1,12 +1,13 @@
+import { useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import ExpensesOutput from '../../components/expenses/output';
+import { ExpensesContext } from '../../store/expenses-context';
 
 const AllExpensesScreen = () => {
-  return <ExpensesOutput period='Total' />;
+  const expensesCtx = useContext(ExpensesContext);
+  return <ExpensesOutput period='Total' expenses={expensesCtx.expenses} />;
 };
 
 export default AllExpensesScreen;
 
-const styles = StyleSheet.create({
-  
-})
+const styles = StyleSheet.create({});
