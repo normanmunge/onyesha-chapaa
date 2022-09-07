ce82a24d6bd75248622c5d7e6e79ba31a75c8c9d
Norman Munge <normanmunge@gmail.com>
2022-09-07 08:00:20 +0300

    Gets recent expenses from expenses context

--- screens/expenses/recent.tsx
+++ screens/expenses/recent.tsx
@@ -1,7 +1,17 @@
+import { useContext } from 'react';
 import ExpensesOutput from '../../components/expenses/output';
+import { ExpensesContext } from '../../store/expenses-context';
+import { getDateMinusDays } from '../../utils/date';
 
 const RecentExpensesScreen = () => {
-  return <ExpensesOutput period='Last 7 days' />;
+  const recentExpensesCtx = useContext(ExpensesContext);
+  const recenteExpenses = recentExpensesCtx.expenses.filter((expense) => {
+    const today = new Date();
+    const lastSevenDays = getDateMinusDays(today, 7);
+
+    return expense['date'] > lastSevenDays;
+  });
+  return <ExpensesOutput expenses={recenteExpenses} period='Last 7 days' />;
 };
 
 export default RecentExpensesScreen;
