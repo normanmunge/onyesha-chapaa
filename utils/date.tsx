c622f4e562a168337c330d4bf66684c0b8f316b1
Norman Munge <normanmunge@gmail.com>
2022-09-07 08:20:30 +0300

    Fixes date bug displaying wrong days

--- utils/date.tsx
+++ utils/date.tsx
@@ -1,7 +1,7 @@
 export function getFormattedDate(date: Date) {
   const month =
     date.getMonth() + 1 > 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`;
-  const day = date.getDay() > 9 ? date.getDay() : `0${date.getDay()}`;
+  const day = date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`;
   return `${date.getFullYear()}-${month}-${day}`;
 }
 
