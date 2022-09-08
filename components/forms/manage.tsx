00ed8257e2bef6324c0c9bf01799edfd01fb6c48
Norman Munge <normanmunge@gmail.com>
2022-09-08 14:38:11 +0300

    Adds generic data binding and input handling in managing form

--- components/forms/manage.tsx
+++ components/forms/manage.tsx
@@ -1,31 +1,58 @@
-import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 import Input from '../ui/input';
 
 const ExpenseForm = () => {
-  const amountChangedHandler = () => {};
+  const [inputValues, setInputValues] = useState({
+    amount: '',
+    date: '',
+    description: '',
+  });
+
+  const { inputsRow, rowInput, form, title } = styles;
+  const inputChangedHandler = (
+    inputIdentifier: string,
+    enteredValue: string
+  ) => {
+    setInputValues((currInputValues) => {
+      return {
+        ...currInputValues,
+        [inputIdentifier]: enteredValue,
+      };
+    });
+  };
 
   return (
-    <View>
-      <Input
-        label='Amount'
-        inputConfig={{
-          keyboadType: 'decimal-pad',
-          onChangeText: amountChangedHandler,
-        }}
-      />
-      <Input
-        label='Date'
-        inputConfig={{
-          placeholder: 'YYYY-MM-DD',
-          maxLength: 10,
-          onChangeText: () => {},
-        }}
-      />
+    <View style={form}>
+      <Text style={title}>Your Expense</Text>
+      <View style={inputsRow}>
+        <Input
+          label='Amount'
+          inputConfig={{
+            keyboadType: 'decimal-pad',
+            onChangeText: inputChangedHandler.bind(this, 'amount'), //binding enables executing a function at a future time
+            value: inputValues.amount,
+          }}
+          style={rowInput}
+        />
+        <Input
+          label='Date'
+          inputConfig={{
+            placeholder: 'YYYY-MM-DD',
+            maxLength: 10,
+            onChangeText: inputChangedHandler.bind(this, 'date'),
+            value: inputValues.date,
+          }}
+          style={rowInput}
+        />
+      </View>
       <Input
         label='Description'
         inputConfig={{
           multiline: true,
           autCorrect: false,
+          onChangeText: inputChangedHandler.bind(this, 'description'),
+          value: inputValues.description,
         }}
       />
     </View>
@@ -34,4 +61,22 @@ const ExpenseForm = () => {
 
 export default ExpenseForm;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+    marginVertical: 24,
+    textAlign: 'center',
+  },
+  inputsRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  rowInput: {
+    flex: 1,
+  },
+});
