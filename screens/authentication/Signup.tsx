2911adeb4dff2129a5b630b3df40e25499e83eda
Norman Munge <normanmunge@gmail.com>
2022-09-18 13:46:44 +0200

    Adds singuphandler for creating new users

--- screens/authentication/Signup.tsx
+++ screens/authentication/Signup.tsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import { View, Text } from 'react-native';
+import AuthContent from '../../components/auth/AuthContent';
+import { createUser } from '../../services/auth';
 
 const SingupScreen = () => {
+
+  const [isAuthenticating, setIsAuthenticating] = useState<boolean>(false);
+
+async function singupHandler({ email, password }: { email: string, password: string }) {
+  setIsAuthenticating(true);
+  await createUser(email, password);
+  setIsAuthenticating(false);
+}
+  
+  if (isAuthenticating) {
+    //TODO: //ADD A ALOADING INDICATOR
+  return <Text>Crating user ...</Text>
+  }
+
   return (
-    <View>
-      <Text>This is the screen to signup a user</Text>
-    </View>
+    <AuthContent onAuthenticate={singupHandler} />
   );
 };
 
