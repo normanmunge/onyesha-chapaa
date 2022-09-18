3d3b80c534630106e9d069f6daee263a86b84b26
Norman Munge <normanmunge@gmail.com>
2022-09-18 13:46:21 +0200

    Adds route type check for navigation in auth stack

--- navigation/auth.navigator.tsx
+++ navigation/auth.navigator.tsx
@@ -4,8 +4,16 @@ import { theme } from '../constants/theme';
 import LoginScreen from '../screens/authentication/Login';
 import SingupScreen from '../screens/authentication/Signup';
 import RecentExpensesScreen from '../screens/expenses/recent';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
-const Stack = createNativeStackNavigator();
+type AuthStackParamList = {
+  Login: undefined;
+  Signup: undefined;
+}
+
+//type AuthProps = NativeStackScreenProps<AuthStackParamList, 'Login'>;
+
+const Stack = createNativeStackNavigator<AuthStackParamList>();
 
 const AuthStack = () => {
   return (
@@ -14,6 +22,7 @@ const AuthStack = () => {
         headerStyle: { backgroundColor: theme.colors.primary },
         headerTintColor: theme.colors.white,
       }}
+      initialRouteName="Login"
     >
       <Stack.Screen name='Login' component={LoginScreen} />
       <Stack.Screen name='Signup' component={SingupScreen} />
@@ -28,7 +37,7 @@ const AuthenticatedStack = () => {
       headerTintColor: theme.colors.white,
     }}
   >
-    <Stack.Screen name='Recent Expenses' component={RecentExpensesScreen} />
+    <Stack.Screen name='RecentExpenses' component={RecentExpensesScreen} />
   </Stack.Navigator>;
 };
 
